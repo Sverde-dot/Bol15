41dc8dbd15168f4fd54f51613d120433a20e5a2b
Sverde-dot <55793525+Sverde-dot@users.noreply.github.com>
2020-01-21 08:49:21 +0100

    Add files via upload

---
+++ notaprimertrimestre/NotaPrimerTrimestre.java
@@ -0,0 +1,14 @@
+
+package notaprimertrimestre;
+
+/**
+ *
+ * @author irodriguezsteuerberg
+ */
+public class NotaPrimerTrimestre {
+
+    public static void main(String[] args) {
+NotaMedia nota1=new NotaMedia();
+nota1.programa();
+    }    
+}
